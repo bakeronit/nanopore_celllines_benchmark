f12df8b22b3dbd359fb8112e05889dad2abfc507
bakeronit <bakeronit@gmail.com>
2025-08-28 11:11:40 +1000

    Revision: update recall and precision with benchmarking with non-standard chromosomes

--- 1.dna_mixing_celllines/R_analysis/config.R
+++ 1.dna_mixing_celllines/R_analysis/config.R
@@ -11,4 +11,6 @@ ggsci_pals <- c("#E64B35FF", "#4DBBD5FF", "#00A087FF")
 colorblind_pals <- friendly_pals$contrast_three
 
 public_tool_names = c("clairS" = "ClairS", "deepsomatic" = "DeepSomatic", "delly"="Delly","nanomonsv" = "nanomonsv", "savana" = "SAVANA", "severus"= "Severus")
-valid_chr <- paste0("chr",c(1:22,"X","Y"))
+valid_chr <- paste0("chr",c(1:22,"X","Y","M"))
+
+revision_workdir <- "/working/lab_nicw/jiaZ/bioprojects/nanopore_celllines_benchmark/x.revision/"
